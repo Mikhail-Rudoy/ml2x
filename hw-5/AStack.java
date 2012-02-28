70fa4979ddeb8e3a637a17965a0dd47f222e5ea5
Mikhail Rudoy <mrudoy@gmail.com>
2012-02-28 17:28:13 -0500

    non-generic implementation of stack using arrays; no tests implemented

---
+++ hw-5/AStack.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class AStack
+{
+    private int top;
+    private Object[] arr;
+    
+    public AStack() 
+    {
+	top = 0;
+	arr = new Object[5];
+    }
+    
+    public AStack(Object[] src)
+    {
+	top = src.length;
+	arr = new Object[2 * src.length + 5]
+	for(int i = 0; i < src.length; i++)
+	{
+	    arr[i] = src[i];
+	}
+    }
+    
+    public void push(Object o)
+    {
+	if(top == arr.length)
+	{
+	    expand();
+	}
+	arr[top++] = o;
+    }
+    
+    public Object pop()
+    {
+	Object o = peek();
+	top = isEmpty() ? 0 : top - 1;
+	return o;
+    }
+    
+    public Object peek()
+    {
+	return isEmpty() ? null : arr[top - 1];
+    }
+    
+    public boolean isEmpty()
+    {
+	return top == 0;
+    }
+    
+    private void expand() 
+    {
+	Object[] n = new Object[arr.length * 2 + 1];
+	for(int i = 0; i < arr.length; i++)
+	{
+	    n[i] = arr[i];
+	}
+	arr = n;
+    }
+
+    public static void main(String[] args) 
+    {
+	
+    }
+}
