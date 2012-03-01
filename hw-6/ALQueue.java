e2752672733bc4d2fd2c66968b8e0de189067a88
Mikhail Rudoy <mrudoy@gmail.com>
2012-03-01 10:51:25 -0500

    ALQueue.java finished and tested including generics but not O(1) implementation of dequeue

---
+++ hw-6/ALQueue.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class ALQueue<Type>
+{
+    ArrayList<Type> AL;
+    
+    public ALQueue()
+    {
+	AL = new ArrayList<Type>();
+    }
+    
+    public void enqueue(Type o)
+    {
+	AL.add(o);
+    }
+    
+    public Type dequeue()
+    {
+	return isEmpty() ? null : AL.remove(0);
+    }
+    
+    public Type peek()
+    {
+	return isEmpty() ? null : AL.get(0);
+    }
+    
+    public boolean isEmpty()
+    {
+	return AL.isEmpty();
+    }
+    
+    public static void main(String[] args)
+    {
+	ALQueue<String> Q = new ALQueue<String>();
+	System.out.println(Q.peek());
+	Q.enqueue("hi");
+	Q.enqueue("yo");
+	System.out.println(Q.isEmpty());
+	System.out.println(Q.peek());
+	System.out.println(Q.dequeue());
+	System.out.println(Q.dequeue());
+	System.out.println(Q.isEmpty());
+    }
+}
