de8bd36f4a2e2f5621a1821282541d63afdab100
Mikhail Rudoy <mrudoy@gmail.com>
2012-03-08 11:46:43 -0500

    hw-8 started

---
+++ hw-8/LList.java
@@ -0,0 +1,146 @@
+/*========== LList.java ==========
+ 
+  An implementation of a simple linked list
+
+  Note that all the methods are the same as the
+  methods of ArrayList. They should exhibit the same 
+  behavior as well.
+
+  jdyrlandweaver
+  =========================*/
+
+import java.io.*;
+import java.util.*;
+
+public class LList<E>
+{
+    private LNode<E> first;
+    private int size;
+    
+    /*======== public LList()) ==========
+      Inputs:   
+      Returns: 
+      
+      Initialize an empty list. 
+      You can use a "dummy" node as the first element
+      to make adding and removing easier.
+
+      03/07/12 09:51:18
+      jdyrlandweaver
+      ====================*/
+    public LList()
+    {
+	first = new LNode<E>( null );
+	size = 0;
+    }
+    
+    /*======== public void add() ==========
+      Inputs:   E data  
+      Returns: 
+
+      Adds a node containing data to the end of the list
+      
+      03/07/12 09:52:29
+      jdyrlandweaver
+      ====================*/
+    public void add(E data)
+    {
+	LNode<E> ln = new LNode<E>(data);
+	LNode<E> current = first;
+	while(current.getNext() != null)
+	{
+	    current = current.getNext();
+	}
+	current.setNext(ln);
+	size++;
+    }
+    
+    /*======== public void add() ==========
+      Inputs:   int index
+                E data  
+      Returns: 
+      
+      Adds a node containing data to position index.
+      If index < 0 or index > size throw an IndexOutOfBoundsException
+      To throw an index, just use the following line:
+          throw new IndexOutOfBoundsException()
+
+      03/07/12 09:53:34
+      jdyrlandweaver
+      ====================*/
+    public void add(int index, E data)
+    {
+	
+    }
+    
+    /*======== public E remove() ==========
+      Inputs:   int index  
+      Returns: The data at index 
+
+      Removes the node at index from the list and returns
+      the data contained in that node
+
+      If index < 0 or index >= size throws an IndexOutOfBoundsException
+
+      03/07/12 09:56:10
+      jdyrlandweaver
+      ====================*/
+    public E remove(int index)
+    {
+	
+    }
+    
+    /*======== public E get() ==========
+      Inputs:   int index  
+      Returns: The data at index
+
+      If index < 0 or index >= size throws an IndexOutOfBoundsException
+
+      03/07/12 09:57:16
+      jdyrlandweaver
+      ====================*/
+    public E get(int index)
+    {
+	
+    }
+    
+    /*======== public void set() ==========
+      Inputs:   int index
+                E data  
+      Returns: 
+
+      Changes the node at index to contain data
+
+      If index < 0 or index >= size throws an IndexOutOfBoundsException
+
+      03/07/12 09:57:42
+      jdyrlandweaver
+      ====================*/
+    public void set(int index, E data)
+    {
+	
+    }
+    
+    /*======== public int size()) ==========
+      Inputs:   
+      Returns: The number of elements in the list
+
+
+      03/07/12 09:58:29
+      jdyrlandweaver
+      ====================*/
+    public int size()
+    {
+	
+    }
+    
+    public String toString()
+    {
+	
+    }
+    
+    public static void main(String[] args)
+    {
+	
+    }
+}
---
+++ hw-8/LNode.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class LNode<E>
+{
+    private E data;
+    private LNode<E> next;
+    
+    public LNode(E d)
+    {
+	data = d;
+	next = null;
+    }
+    
+    public E getData()
+    {
+	return data;
+    }
+    
+    public void setData(E d)
+    {
+	data = d;
+    }
+
+    public LNode<E> getNext()
+    {
+	return next;
+    }
+    
+    public void setNext(LNode<E> n)
+    {
+	next = n;
+    }
+}
